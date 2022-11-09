c56fb43e7ac2685d200cff60e836ab4a29508bbb
Haowe <haowen.liu@epfl.ch>
2022-11-10 00:24:12 +0100

    modify run file

---
+++ project/DNS_server.py
@@ -0,0 +1,39 @@
+from dnslib import dns as dd
+from dnslib import server as ds
+from threading import Thread
+
+
+class DNS_resolver:
+    def __init__(self):
+        self.zones = []
+
+    def zone_add_A(self, domain, ip):
+        self.zones.append(dd.RR(domain, dd.QTYPE.A, rdata=dd.A(ip), ttl=300))
+
+    def zone_add_TXT(self, domain, txt):
+        self.zones.append(dd.RR(domain, dd.QTYPE.TXT, rdata=dd.TXT(txt), ttl=300))
+
+    def resolve(self, request, handler):
+        reply = request.reply()
+        for zone in self.zones:
+            reply.add_answer(zone)
+
+
+class DNS_server:
+    def __init__(self):
+        self.resolver = DNS_resolver()
+        self.logger = ds.DNSLogger("request,reply,truncated,error", False)
+        self.server = ds.DNSServer(self.resolver, port=10053, logger=self.logger)
+
+    def zone_add_A(self, domain, ip):
+        self.resolver.zone_add_A(domain, ip)
+
+    def zone_add_TXT(self, domain, txt):
+        self.resolver.zone_add_TXT(domain, txt)
+
+    def server_run(self):
+        self.runserver = Thread(target=self.server.start)
+        self.runserver.start()
+
+    def server_shut(self):
+        self.server.stop()
