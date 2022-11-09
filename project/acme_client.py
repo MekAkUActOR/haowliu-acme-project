cf4c120a7846e8f4304e6134f26c2d42bd16d10c
Haowe <haowen.liu@epfl.ch>
2022-11-10 00:11:04 +0100

    tests

---
+++ project/acme_client.py
@@ -0,0 +1,344 @@
+"""Referenced a bit from https://github.com/diafygi/acme-tiny and https://github.com/mpdavis/python-jose,
+design implementations are different"""
+
+import argparse
+import subprocess, json, os, sys
+import base64, binascii, time, hashlib, re
+import copy, textwrap, logging
+from datetime import datetime, timedelta, timezone
+
+from httpchallenge_server import register_http_challenge
+
+from Crypto.PublicKey import ECC
+from Crypto.Signature import DSS
+from Crypto.Hash import SHA256
+
+try:
+    import requests
+except ImportError:
+    print("error in importing requests, install with pip")
+    sys.exit(2)
+
+from requests.models import Response
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+class ACMEClient():
+
+    def __init__(self, directory, dns_server):
+
+        self.directory = directory
+        self.dns_server = dns_server
+
+        # Required paramters in the JSON object in the ACME directory
+        self.revokeCert_url = None
+        self.newNonce_url = None
+        self.newAccount_url = None
+        self.newOrder_url = None
+
+        self.account_kid = None
+
+        self.key = None
+        self.signing_alg = None
+
+        self.client_session = requests.Session()
+        self.jose_session = requests.Session()
+
+        # For waiting states while finalizing the certificates
+        self.starting_success_states, self.starting_failure_states = ["ready", "processing", "valid"], ["invalid"]
+        self.final_success_states, self.final_failure_states = ["valid"], ["ready", "invalid", "pending"]
+
+        # retry = Retry(connect=3, backoff_factor=0.5)
+
+        # ACME clients MUST send a User-Agent header field, in accordance with [RFC7231]
+        self.client_session.headers.update({"User-Agent": "sidray-acme-project"})
+        self.client_session.mount('https://', HTTPAdapter(max_retries=0))
+
+        self.jose_session.headers.update(
+            {"User-Agent": "sidray-acme-project", "Content-Type": "application/jose+json"})
+        self.jose_session.mount('https://', HTTPAdapter(max_retries=0))
+
+        self.generate_keypair()
+        print("Client keypair generated")
+
+    # Used the pycryptodome documentation
+    # https://pycryptodome.readthedocs.io/en/latest/src/public_key/#available-key-types
+
+    """An ACME server MUST implement the "ES256" signature algorithm
+        [RFC7518] and SHOULD implement the "EdDSA" signature algorithm using
+        the "Ed25519" variant (indicated by "crv") [RFC8037]."""
+
+    def generate_keypair(self):
+
+        self.key = ECC.generate(curve="p256")
+
+        '''f = open("private_key.pem", "wt")
+        f.write(self.key.export_key(format='PEM'))
+        f.close()'''
+
+        self.signing_alg = DSS.new(self.key, "fips-186-3")
+
+    # Encode in B64 as per ACME RFC specification
+    # https://stackoverflow.com/questions/23164058/how-to-encode-text-to-base64-in-python
+    def encode_b64(self, data):
+        if isinstance(data, str):
+            data = data.encode('utf-8')
+        return base64.urlsafe_b64encode(data).decode('utf-8').rstrip("=")
+
+    # Get the values from the root ACME directory
+    def get_directory(self):
+
+        directory_request = self.client_session.get(self.directory)
+
+        # Request code 200 for directory
+        if directory_request.status_code == 200:
+            jose_request_object = directory_request.json()
+
+            self.revokeCert_url = jose_request_object["revokeCert"]
+            self.newNonce_url = jose_request_object["newNonce"]
+            self.newAccount_url = jose_request_object["newAccount"]
+            self.newOrder_url = jose_request_object["newOrder"]
+            return jose_request_object
+
+    # Split x and y coordinates : https://openid.net/specs/draft-jones-json-web-signature-04.html#DefiningECDSA
+    # https://pycryptodome.readthedocs.io/en/latest/src/public_key/ecc.html#Crypto.PublicKey.ECC.EccPoint
+    def create_jwk_object(self):
+        jwk_object = {
+            "crv": "P-256",
+            "kid": "1",
+            "kty": "EC",
+            "x": self.encode_b64(self.key.pointQ.x.to_bytes()),
+            "y": self.encode_b64(self.key.pointQ.y.to_bytes()),
+        }
+
+        return jwk_object
+
+    def get_nonce(self):
+        if self.newNonce_url == None:
+            print("get_nonce: URL unknown, directory for nonce is missing")
+            return
+
+        request = self.client_session.get(self.newNonce_url)
+
+        # 204 for no content
+        if request.status_code == 200 or request.status_code == 204:
+            self.nextNonce = request.headers["Replay-Nonce"]
+            return self.nextNonce
+
+    def create_key_authorization(self, token):
+        key = {
+            "crv": "P-256",
+            "kty": "EC",
+            "x": self.encode_b64(self.key.pointQ.x.to_bytes()),
+            "y": self.encode_b64(self.key.pointQ.y.to_bytes()),
+        }
+
+        hash_val = self.encode_b64(SHA256.new(str.encode(
+            json.dumps(key, separators=(',', ':')), encoding="utf-8")).digest())
+        key_auth = "{}.{}".format(token, hash_val)
+
+        return key_auth
+
+    # Method to create the account
+    def create_account(self):
+        payload = {
+            "termsOfServiceAgreed": True,
+        }
+
+        jose_payload = self.create_jose_jwk(self.newAccount_url, payload)
+        jose_request = self.jose_session.post(self.newAccount_url, json=jose_payload)
+
+        # Request code 201 for account creation
+        if jose_request.status_code == 201:
+            jose_request_object = jose_request.json()
+
+            self.account_kid = jose_request.headers["Location"]
+
+            return jose_request_object
+
+    """The "jwk" and "kid" fields are mutually exclusive. Servers MUST
+        reject requests that contain both.
+
+        All the new_accounts must be signed with jwk"""
+
+    def create_jose_jwk(self, url, payload):
+
+        protected = {
+            "alg": "ES256",
+            "jwk": self.create_jwk_object(),
+            "nonce": self.get_nonce(),
+            "url": url
+        }
+
+        encoded_header = self.encode_b64(json.dumps(protected))
+        encoded_payload = self.encode_b64(json.dumps(payload))
+
+        # Create SHA thumbprint
+        # https://pycryptodome.readthedocs.io/en/latest/src/hash/sha256.html
+        hash_val = SHA256.new(str.encode("{}.{}".format(
+            encoded_header, encoded_payload), encoding="ascii"))
+
+        signature = self.signing_alg.sign(hash_val)
+
+        jose_object = {
+            "protected": encoded_header,
+            "payload": encoded_payload,
+            "signature": self.encode_b64(signature)
+        }
+
+        return jose_object
+
+    """ Non new accounts signed with kid"""
+
+    def create_jose_kid(self, url, payload):
+
+        protected = {
+            "alg": "ES256",
+            "kid": self.account_kid,
+            "nonce": self.get_nonce(),
+            "url": url
+        }
+
+        encoded_header = self.encode_b64(json.dumps(protected))
+
+        # Create SHA thumbprint
+        if payload == "":
+            encoded_payload = ""
+            hash_val = SHA256.new(str.encode(
+                "{}.".format(encoded_header), encoding="ascii"))
+        else:
+            encoded_payload = self.encode_b64(json.dumps(payload))
+            hash_val = SHA256.new(str.encode("{}.{}".format(
+                encoded_header, encoded_payload), encoding="ascii"))
+        signature = self.signing_alg.sign(hash_val)
+
+        kid_jose_object = {
+            "protected": encoded_header,
+            "payload": encoded_payload,
+            "signature": self.encode_b64(signature)
+        }
+
+        return kid_jose_object
+
+    def issue_certificate(self, domains, begin=datetime.now(timezone.utc), duration=timedelta(days=365)):
+        payload = {
+            "identifiers": [{"type": "dns", "value": domain} for domain in domains],
+            "notBefore": begin.isoformat(),
+            "notAfter": (begin + duration).isoformat()
+        }
+
+        jose_payload = self.create_jose_kid(self.newOrder_url, payload)
+        response = self.jose_session.post(self.newOrder_url, json=jose_payload)
+
+        if response.status_code == 201:
+            jose_request_object = response.json()
+
+            return jose_request_object, response.headers["Location"]
+
+    def authorize_certificate(self, auth_url, auth_scheme):
+        payload = ""
+
+        jose_payload = self.create_jose_kid(auth_url, payload)
+        request = self.jose_session.post(auth_url, json=jose_payload)
+
+        if request.status_code == 200:
+            jose_request_object = request.json()
+
+            for challenge in jose_request_object["challenges"]:
+                key_auth = self.create_key_authorization(challenge["token"])
+
+                if auth_scheme == "dns01" and challenge["type"] == "dns-01":
+                    key_auth = self.encode_b64(SHA256.new(
+                        str.encode(key_auth, encoding="ascii")).digest())
+
+                    """ For example, if the domain name being validated is
+                    "www.example.org", then the client would provision the following DNS
+                    record:
+                    _acme-challenge.www.example.org. 300 IN TXT "gfj9Xq...Rg85nM"""
+
+                    self.dns_server.zone_add_TXT(
+                        "_acme-challenge.{}".format(jose_request_object["identifier"]["value"]), key_auth)
+                    return challenge
+
+                elif auth_scheme == "http01" and challenge["type"] == "http-01":
+                    register_http_challenge(challenge["token"], key_auth)
+                    return challenge
+
+    # Validate the certificate after the challenge
+    def validate_certificate(self, validate_url):
+        payload = {}
+        jose_payload = self.create_jose_kid(validate_url, payload)
+        response = self.jose_session.post(validate_url, json=jose_payload)
+
+        if response.status_code == 200:
+            jose_request_object = response.json()
+
+            return jose_request_object
+
+    # https://github.com/diafygi/acme-tiny/blob/master/acme_tiny.py
+    def poll_resource_status(self, order_url, success_states, failure_states):
+        while True:
+            payload = ""
+            jose_payload = self.create_jose_kid(order_url, payload)
+            jose_request = self.jose_session.post(order_url, payload, json=jose_payload)
+            jose_request_object = jose_request.json()
+
+            if jose_request.status_code == 200:
+
+                if jose_request_object["status"] in success_states:
+                    print("Resource {} has {} state".format(
+                        order_url, jose_request_object["status"]))
+                    return jose_request_object
+
+                elif jose_request_object["status"] in failure_states:
+                    print("Resource {} has {} state, treated as failure".format(
+                        order_url, jose_request_object["status"]))
+                    return False
+
+            time.sleep(1)
+
+    # For finalising the certificate, wait till it is valid (use the polling function)
+    def finalize_certificate(self, order_url, finalize_url, der):
+        jose_request_object = self.poll_resource_status(
+            order_url, self.starting_success_states, self.starting_failure_states)
+
+        if not jose_request_object:
+            return False
+
+        payload = {
+            "csr": self.encode_b64(der)
+        }
+
+        jose_payload = self.create_jose_kid(finalize_url, payload)
+        response = self.jose_session.post(finalize_url, json=jose_payload)
+
+        if response.status_code == 200:
+            jose_request_object = self.poll_resource_status(
+                order_url, self.final_success_states, self.final_failure_states)
+            if jose_request_object:
+                return jose_request_object["certificate"]
+            else:
+                return False
+
+    # Download the certifcate after finalizing
+    def download_certificate(self, certificate_url):
+        payload = ""
+        jose_payload = self.create_jose_kid(certificate_url, payload)
+
+        response = self.jose_session.post(certificate_url, json=jose_payload)
+        if response.status_code == 200:
+            return response.content
+
+    # Still confused whether kid or jwk must be used for revocation (read this again)
+    # For the moment kid works
+    def revoke_certificate(self, certificate):
+        payload = {
+            "certificate": self.encode_b64(certificate)
+        }
+        jose_payload = self.create_jose_kid(self.revokeCert_url, payload)
+
+        response = self.jose_session.post(self.revokeCert_url, json=jose_payload)
+        if response.status_code == 200:
+            return response.content
+
--- project/main.py
+++ project/main.py
@@ -1,36 +1,227 @@
-import argparse
 import os
-from threading import Thread
-from flask import Flask, request
+import subprocess
+import sys
+import threading
+import time
+from pathlib import Path
+import argparse
 
-from utils import gen_csr_and_key, write_cert, https_with_cert
-# from Shut_HTTP_server import server as shutserver
+import flask
 
-shut_http_server = Flask(__name__)
-@shut_http_server.route('/shutdown')
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from acme_client import ACMEClient
+from dns_server import newDNSServer
+from https_server import start_https_server
+from httpchallenge_server import start_http_challenge_server
+
+
+# https://www.programcreek.com/python/?CodeExample=generate+csr
+def generate_csr_key(domains):
+    key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+
+    csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, "CH"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "ZH"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, "Zurich"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, "sidray-acme-project"),
+        x509.NameAttribute(NameOID.COMMON_NAME, "sidray-acme-project"),
+    ])).add_extension(
+        x509.SubjectAlternativeName([x509.DNSName(domain)
+                                     for domain in domains]),
+        critical=False,
+    ).sign(key, hashes.SHA256())
+
+    der = csr.public_bytes(serialization.Encoding.DER)
+
+    return key, csr, der
+
+
+key_path = Path(__file__).parent.absolute() / "key.pem"
+cert_path = Path(__file__).parent.absolute() / "cert.pem"
+
+
+# write the cert and the pem file for the key
+# https://stackoverflow.com/questions/56285000/python-cryptography-create-a-certificate-signed-by-an-existing-ca-and-export
+def write_certificates(key, certificate):
+    with open(key_path, "wb") as f:
+        f.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption()
+        ))
+
+    with open(cert_path, "wb") as f:
+        f.write(certificate)
+
+
+# Wrapper for obtaining the certificate using ACME client methods
+def obtain_certificate(args):
+    # Create DNS server object
+    dns_server = newDNSServer()
+    # Start the HTTP challenge server
+    start_http_challenge_server()
+
+    # Take care of multiple domains
+    for domain in args.domain:
+        dns_server.zone_add_A(domain, args.record)
+
+    # Start the DNS server thread
+    dns_server.server_start()
+    print("[Get certificate] DNS server started")
+
+    # Create the ACME client and use the methods defined in acme_client.py
+    acme_client = ACMEClient(args.dir, dns_server)
+    if not acme_client:
+        print("Create client error. Process killed.")
+        return False
+
+    directory = acme_client.get_directory()
+    if not directory:
+        print("Get directory error. Process killed.")
+        return False
+
+    print("[Get directory]", directory)
+
+    account = acme_client.create_account()
+    if not account:
+        print("Create account error. Process killed.")
+        return False
+
+    print("[Account created successfully]", account)
+
+    certificate_order, order_url = acme_client.issue_certificate(args.domain)
+
+    if not certificate_order:
+        print("Certificate order error. Process killed.")
+        return False
+
+    print("[Placed order successfully]", certificate_order)
+
+    validate_urls = []
+    finalize_url = certificate_order["finalize"]
+
+    for auth in certificate_order["authorizations"]:
+        certificate_authorization = acme_client.authorize_certificate(auth, args.challenge)
+
+        if not certificate_authorization:
+            print("Certificate authentication error. Process killed")
+            return False
+        validate_urls.append(certificate_authorization["url"])
+
+        print("[Authorized certificate successfully]", certificate_authorization)
+
+    for url in validate_urls:
+        certificate_valid = acme_client.validate_certificate(url)
+
+        if not certificate_valid:
+            print("Certificate validation error. Process killed")
+            return False
+
+        print("[Validated certificate sucessfully]", certificate_valid)
+
+    key, csr, der = generate_csr_key(args.domain)
+
+    certificate_url = acme_client.finalize_certificate(order_url, finalize_url, der)
+    if not certificate_url:
+        print("Certificate finalizing error. Process killed")
+        return False
+
+    print("[Finalized certificate]", certificate_url)
+
+    downloaded_certificate = acme_client.download_certificate(certificate_url)
+
+    if not downloaded_certificate:
+        print("Certificate downloading error. Process killed.")
+        return False
+
+    print("[Certificate ready for download]", downloaded_certificate)
+
+    # Write certifcate and key to files, loaded later for use.
+    write_certificates(key, downloaded_certificate)
+
+    # https://cryptography.io/en/latest/x509/reference/
+    crypto_certificate = x509.load_pem_x509_certificate(downloaded_certificate)
+
+    if args.revoke:
+        acme_client.revoke_certificate(crypto_certificate.public_bytes(serialization.Encoding.DER))
+
+    return key, downloaded_certificate
+
+
+# Method to exit if no key found
+def kill_all_processes():
+    os._exit(0)
+
+
+# Method to start https server to use the certificate
+def start_server_to_use_certificate(args):
+    # Get certificate, however I will read the certificate from the stored file
+    key, cert = obtain_certificate(args)
+
+    # This still happens locally, because pebble rejects 5% of the nonces
+    if not key:
+        print("No key found, not starting the https server, killing all..")
+        kill_all_processes()
+
+    # Trying to kill the DNS server for the invalid certificate test
+    os.system("pkill -f dns_server.py")
+
+    # Start HTTPS server to use the certificate
+    start_https_server(key_path, cert_path)
+
+
+# Shutdown server created inside the main script
+httpshutdown_server = flask.Flask(__name__)
+
+
+@httpshutdown_server.route('/shutdown')
 def route_shutdown():
     print("Shutting down...")
-    func = request.environ.get('werkzeug.server.shutdown')
+
+    # https://stackoverflow.com/questions/15562446/how-to-stop-flask-application-without-using-ctrl-c
+    func = flask.request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
     return "Server shutting down"
 
+
+# Controller (still used to my SDN terminology haha) to initiate the certificate process, and start the https server at the end
+def controller(args):
+    controller_thread = threading.Thread(target=lambda: httpshutdown_server.run(
+        host="0.0.0.0", port=5003, debug=False, threaded=True))
+    controller_thread.start()
+
+    https_server_thread = threading.Thread(target=lambda: start_server_to_use_certificate(args))
+    https_server_thread.start()
+
+    # Kill application when the controller terminates
+    controller_thread.join()
+    kill_all_processes()
+
+
 def main():
-    parser = argparse.ArgumentParser(description="ACME Project")
-    parser.add_argument("cha_type", choices=["dns01", "http01"])
-    parser.add_argument("--dir", required=True)
-    parser.add_argument("--record", required=True)
-    parser.add_argument("--domain", required=True, action="append")
-    parser.add_argument("--revoke", action="store_true")
+    parser = argparse.ArgumentParser(description="sidray-acme-project parser")
+    parser.add_argument("challenge", choices=["dns01", "http01"])
+    parser.add_argument("--dir", help="the directory URL of the ACME server that should be used", required=True)
+    parser.add_argument("--record", required=True,
+                        help="the IPv4 address which must be returned by your DNS server for all A-record queries")
+    parser.add_argument("--domain", action="append",
+                        help="the domain for  which to request the certificate. If multiple --domain flags are present, a single certificate for multiple domains should be requested. Wildcard domains have no special flag and are simply denoted by, e.g., *.example.net")
+    parser.add_argument("--revoke", action="store_true",
+                        help="Immediately revoke the certificate after obtaining it. In both cases, your application should start its HTTPS server and set it up to use the newly obtained certificate.")
+
     args = parser.parse_args()
-    main_th = Thread(target=lambda: shut_http_server.run(host="0.0.0.0", port=5003, debug=False, threaded=True))
-    main_th.start()
-    https_th = Thread(target=lambda: https_with_cert(args.cha_type, args.dir, args.record, args.domain, args.revoke))
-    https_th.start()
-    main_th.join()
-    os._exit(0)
+
+    print("Parsed arguments: ", args)
+
+    controller(args)
+
 
 if __name__ == "__main__":
     main()
