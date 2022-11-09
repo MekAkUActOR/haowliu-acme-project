a300edb1401bc65a5d4897f511303a79331f9488
Haowe <haowen.liu@epfl.ch>
2022-11-09 21:12:04 +0100

    fix flask reloader

--- project/Cha_HTTP_server.py
+++ project/Cha_HTTP_server.py
@@ -4,7 +4,7 @@ server = Flask(__name__)
 from threading import Thread
 
 auths = {}
-@server.route("/.well-known/acme-challenge/<string:token>")
+@server.route('/.well-known/acme-challenge/<string:token>')
 def cha_http(token):
     if token in auths:
         resp = Response(auths[token], mimetype="application/octet-stream")
@@ -16,5 +16,5 @@ def reg_httpcha(token, auth):
     auths[token] = auth
 
 def cha_http_server():
-    runserver = Thread(target=lambda : server.run(host="0.0.0.0", port=5002, debug=True, threaded=True))
+    runserver = Thread(target=lambda: server.run(host="0.0.0.0", port=5002, debug=True, threaded=True, use_reloader=False))
     runserver.start()
