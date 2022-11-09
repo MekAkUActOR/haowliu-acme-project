a898c29d7123ff425644ac2e2248b7c0badeaaa9
Haowe <haowen.liu@epfl.ch>
2022-11-10 00:57:00 +0100

    fix bugs in shutserver

--- project/Shut_HTTP_server.py
+++ project/Shut_HTTP_server.py
@@ -1,19 +1,12 @@
 from flask import Flask, request
 server = Flask(__name__)
 
-from threading import Thread
-
-def shutdown_server():
+@server.route('/shutdown')
+def route_shutdown():
+    print("Shutting down...")
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
-@server.route('/shutdown')
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
-
-def shut_http_server():
-    runserver = Thread(target=lambda: server.run(host="0.0.0.0", port=5003, debug=False, threaded=True))
-    runserver.start()
+    return "Server shutting down"
