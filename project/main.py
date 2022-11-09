6c9e8d6e6af48e60bb0321fc66b3a03dc1634826
Haowe <haowen.liu@epfl.ch>
2022-11-09 20:48:10 +0100

    modify shutdown server

--- project/main.py
+++ project/main.py
@@ -1,11 +1,23 @@
 import argparse
 import os
 from threading import Thread
+from flask import Flask, request
 from pathlib import Path
 import cryptography
 
 from utils import gen_csr_and_key, write_cert, https_with_cert
-from Shut_HTTP_server import server as shutserver
+# from Shut_HTTP_server import server as shutserver
+
+shut_http_server = Flask(__name__)
+@shut_http_server.route('/shutdown')
+def route_shutdown():
+    print("Shutting down...")
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+    return "Server shutting down"
 
 def main():
     parser = argparse.ArgumentParser(description="ACME Project")
@@ -15,7 +27,7 @@ def main():
     parser.add_argument("--domain", required=True, action="append")
     parser.add_argument("--revoke", action="store_true")
     args = parser.parse_args()
-    main_th = Thread(target=lambda : shutserver.run(host="0.0.0.0", port=5003, debug=True, threaded=True))
+    main_th = Thread(target=lambda : shut_http_server.run(host="0.0.0.0", port=5003, debug=True, threaded=True))
     main_th.start()
     https_th = Thread(target=lambda : https_with_cert(args.cha_type, args.dir, args.record, args.domain, args.revoke))
     https_th.start()
