b2ddb491f1cd372b278339f9ea759b9a0d789b4a
Haowe <haowen.liu@epfl.ch>
2022-11-11 03:18:46 +0100

    issue cert time format

--- project/ACME_client.py
+++ project/ACME_client.py
@@ -40,85 +40,85 @@ class ACME_client():
 
     def create_account(self):
         payload = {"termsOfServiceAgreed": True}
-        jose_payload = self.create_jose_jwk(self.dir_obj["newAccount"], payload)
-        jose_request = self.client_s.post(self.dir_obj["newAccount"], json=jose_payload, headers=jose_header)
 
-        if jose_request.status_code == 201:
-            jose_request_obj = jose_request.json()
-            self.account_kid = jose_request.headers["Location"]
-            return jose_request_obj
-
-    def create_key_auth(self, token):
-        key = {
-            "crv"   :   "P-256",
-            "kty"   :   "EC",
-            "x"     :   b64encode(self.key.pointQ.x.to_bytes()),
-            "y"     :   b64encode(self.key.pointQ.y.to_bytes()),
-        }
-        hash_value = b64encode(hash(json.dumps(key, separators=(',',':')), "utf-8").digest())
-        key_auth = "{}.{}".format(token, hash_value)
-        return key_auth
-
-    def create_jose_jwk(self, url, payload):
         protected = {}
         protected["alg"] = "ES256"
         protected["jwk"] = {
-            "crv"   :   "P-256",
-            "kty"   :   "EC",
-            "x"     :   b64encode(self.key.pointQ.x.to_bytes()),
-            "y"     :   b64encode(self.key.pointQ.y.to_bytes()),
+            "crv": "P-256",
+            "kty": "EC",
+            "x": b64encode(self.key.pointQ.x.to_bytes()),
+            "y": b64encode(self.key.pointQ.y.to_bytes()),
         }
         protected["nonce"] = self.get_nonce()
-        protected["url"] = url
+        protected["url"] = self.dir_obj["newAccount"]
 
         encoded_header = b64encode(json.dumps(protected))
         encoded_payload = b64encode(json.dumps(payload))
         hash_value = hash("{}.{}".format(encoded_header, encoded_payload), "ascii")
         signature = b64encode(self.sign_alg.sign(hash_value))
-        jose_obj = {
-            "protected" :   encoded_header,
-            "payload"   :   encoded_payload,
-            "signature" :   signature,
-        }
-        return jose_obj
 
-    def create_jose_kid(self, url, payload):
-        protected = {
-            "alg"   :   "ES256",
-            "kid"   :   self.account_kid,
-            "nonce" :   self.get_nonce(),
-            "url"   :   url,
+        jose_payload = {
+            "protected": encoded_header,
+            "payload": encoded_payload,
+            "signature": signature,
         }
-        encoded_header = b64encode(json.dumps(protected))
+        jose_resp = self.client_s.post(self.dir_obj["newAccount"], json=jose_payload, headers=jose_header)
+
+        if jose_resp.status_code == 201:
+            jose_resp_obj = jose_resp.json()
+            self.account_kid = jose_resp.headers["Location"]
+            return jose_resp_obj
+
+    def create_key_auth(self, token):
+        key = {
+            "crv": "P-256",
+            "kty": "EC",
+            "x": b64encode(self.key.pointQ.x.to_bytes()),
+            "y": b64encode(self.key.pointQ.y.to_bytes()),
+        }
+        hash_value = b64encode(hash(json.dumps(key, separators=(',',':')), "utf-8").digest())
+        key_auth = "{}.{}".format(token, hash_value)
+        return key_auth
+
+    def package_payload(self, url, payload):
+        protected = {
+            "alg": "ES256",
+            "kid": self.account_kid,
+            "nonce": self.get_nonce(),
+            "url": url,
+        }
+        encoded_protected = b64encode(json.dumps(protected))
 
         if payload == "":
             encoded_payload = ""
-            hash_value = hash("{}.".format(encoded_header), "ascii")
+            hash_value = hash("{}.".format(encoded_protected), "ascii")
         else:
             encoded_payload = b64encode(json.dumps(payload))
-            hash_value = hash("{}.{}".format(encoded_header, encoded_payload), "ascii")
-        signature = self.sign_alg.sign(hash_value)
+            hash_value = hash("{}.{}".format(encoded_protected, encoded_payload), "ascii")
+        signature = b64encode(self.sign_alg.sign(hash_value))
         return {
-            "protected" :   encoded_header,
-            "payload"   :   encoded_payload,
-            "signature" :   b64encode(signature),
+            "protected": encoded_protected,
+            "payload": encoded_payload,
+            "signature": signature,
         }
 
-    def issue_cert(self, domains, begin=datetime.datetime.now(datetime.timezone.utc), duration=datetime.timedelta(days=365)):
+    def issue_cert(self, domains):
+        begin = datetime.datetime.now(datetime.timezone.utc)
+        duration = datetime.timedelta(days=365)
         payload = {
             "identifiers"   :   [{"type":"dns","value":domain} for domain in domains],
             "notBefore"     :   begin.isoformat(),
             "notAfter"      :   (begin + duration).isoformat(),
         }
-        jose_payload = self.create_jose_kid(self.dir_obj["newOrder"], payload)
-        response = self.client_s.post(self.dir_obj["newOrder"], json=jose_payload, headers=jose_header)
-        if response.status_code == 201:
-            jose_request_obj = response.json()
-            return jose_request_obj, response.headers["Location"]
+        jose_payload = self.package_payload(self.dir_obj["newOrder"], payload)
+        resp = self.client_s.post(self.dir_obj["newOrder"], json=jose_payload, headers=jose_header)
+        if resp.status_code == 201:
+            order_obj = resp.json()
+            return order_obj, resp.headers["Location"]
 
     def auth_cert(self, auth_url, auth_scheme, cha_server, dns_server):
         payload = ""
-        jose_payload = self.create_jose_kid(auth_url, payload)
+        jose_payload = self.package_payload(auth_url, payload)
         request = self.client_s.post(auth_url, json=jose_payload, headers=jose_header)
         if request.status_code == 200:
             jose_request_obj = request.json()
@@ -134,7 +134,7 @@ class ACME_client():
 
     def vali_cert(self, vali_url):
         payload = {}
-        jose_payload = self.create_jose_kid(vali_url, payload)
+        jose_payload = self.package_payload(vali_url, payload)
         response = self.client_s.post(vali_url, json=jose_payload, headers=jose_header)
         if response.status_code == 200:
             jose_request_obj = response.json()
@@ -143,7 +143,7 @@ class ACME_client():
     def poll_resource_status(self, order_url, success_states, failure_states):
         while True:
             payload = ""
-            jose_payload = self.create_jose_kid(order_url, payload)
+            jose_payload = self.package_payload(order_url, payload)
             jose_request = self.client_s.post(order_url, payload, json=jose_payload, headers=jose_header)
             jose_request_obj = jose_request.json()
             if jose_request.status_code == 200:
@@ -160,7 +160,7 @@ class ACME_client():
         if not jose_request_obj:
             return False
         payload = {"csr": b64encode(der)}
-        jose_payload = self.create_jose_kid(fin_url, payload)
+        jose_payload = self.package_payload(fin_url, payload)
         response = self.client_s.post(fin_url, json=jose_payload, headers=jose_header)
         if response.status_code == 200:
             jose_request_obj = self.poll_resource_status(order_url, ["valid"], ["ready", "invalid", "pending"])
@@ -171,14 +171,14 @@ class ACME_client():
 
     def dl_cert(self, cert_url):
         payload = ""
-        jose_payload = self.create_jose_kid(cert_url, payload)
+        jose_payload = self.package_payload(cert_url, payload)
         response = self.client_s.post(cert_url, json=jose_payload, headers=jose_header)
         if response.status_code == 200:
             return response.content
 
     def revoke_cert(self, cert):
         payload = {"certificate": b64encode(cert)}
-        jose_payload = self.create_jose_kid(self.dir_obj["revokeCert"], payload)
+        jose_payload = self.package_payload(self.dir_obj["revokeCert"], payload)
         response = self.client_s.post(self.dir_obj["revokeCert"], json=jose_payload, headers=jose_header)
         if response.status_code == 200:
             return response.content
